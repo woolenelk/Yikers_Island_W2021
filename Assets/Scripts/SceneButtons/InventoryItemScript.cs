8084e4c2d9afa640cbf634e8f487435bc8c63158
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-02-06 16:50:17 -0500

    added click register to tower inventory

---
+++ Assets/Scripts/SceneButtons/InventoryItemScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryItemScript : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Tower Selected");
+    }
+
+    
+}
